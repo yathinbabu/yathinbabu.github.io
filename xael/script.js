48e09b2886043a7585bbc9d6ba4d7981488ebc73
Yathin Babu N <anythingicode@gmail.com>
2020-04-29 12:15:19 +0530

    bug fix

--- xael/script.js
+++ xael/script.js
@@ -47,49 +47,62 @@ $(function() {
      10>h?h="0"+h:h=h;
      10>t?t="0"+t:t=t;
      12>h?time=h+":"+t+" am":time=(h-12)+":"+t+" pm";
-     var hello=["HELLO","HI","HEY THERE","HEY","HI XEAL","HELLO XEAL","HEY THERE","HEY XEAL"];
-     var gm=["GM","GOOD MORNING","GOOD MORNING Xael", "GOOD MORNING Xael"];
-     var bad=["BAD","NOT BAD","USELESS","NOT WORKING","NOT GOOD"];
-     var ge=["GOOD EVNG","GOOD EVENING","GOOD EVENING Xael","GOOD EVENING Xael"];
-     var gn=["GOON NIGHT","I'M FEELING SLEEPY"];
-     var welcome=["NICE WORKING", "NICE CODE","NICE","WOW","WOW IT'S WORKING","GREAT CODE","AWESOME CODE","IT'S NICE","AWESOME CODE BRO","IT'S GOOD","OH MY GOD","OMG","OHO","NICE TO MEET YOU","NICE TO MEET U","NICE TO SEE YOU","NICE TO C U"];
-     var s2u=["I HATE YOU","I LOVE YOU","I MISS YOU"];
-     var gaf=["GOOD AFTERNOON","GOOD AFTERNOON Xael","GOOD AFTERNOON Xael" ];
+     var hello=["HELLO","HI","HEY THERE","HEY","HI XAEL","HELLO XAEL","HI XAEL","HEY XAEL"];
+     var gm=["GM","GM XAEL","GOOD MORNING","GOOD MORNING XAEL"];
+     var bad=["YOU ARE NOT WORKING","YOU ARE NOT GOOD","YOU ARE BAD","YOU ARE USELESS"];
+     var ge=["GOOD EVNG","GOOD EVENING","GOOD EVENING XAEL","GOOD EVNG XAEL","GE XAEL"];
+     var gn=["GOON NIGHT","I'M FEELING SLEEPY","GN","GOON NIGHT XAEL","GN XAEL"];
+     var welcome=["YOU ARE GOOD","YOU ARE AWESOME","YOU ARE EXCELLENT","Y'R GOOD","NICE TO MEET YOU","NICE TO MEET U","NICE TO SEE YOU","NICE TO C U","NICE TO MEET YOU TOO"];
+     var s2u=["I LOVE YOU","I MISS YOU"];
+     var gaf=["GOOD AFTERNOON","GOOD AFTERNOON XAEL","GOOD AFTERNOON XAEL","GA XAEL"];
      var like=["GOOD","👍🏻", "☺️","😅","LOL","ME TOO"];
      var hru=["HOW R U?","H R U?", "HOW ARE YOU?", "HRU?","HRU", "HOW R U","H R U", "HOW ARE YOU"];
      var good=["I'M GOOD","I'M FINE", "I'M FINE U", "I AM FINE","I'M FINE AND HOW ARE YOU", "MEE TOO", "FINE","FINE:)","FINE :)", "MARVELOUS","AWESOME","FINE☺️","GREAT","I'M FINE, HOW ARE YOU?","IM FINE","IM GOOD","IM AWESOME"];
      var mood=["IM FINE, HOW ARE YOU?","IM GOOD, HOW ARE YOU?","IM AWESOME, HOW ARE YOU?","IM FINE HOW ARE YOU?","IM GOOD HOW ARE YOU?","IM AWESOME HOW ARE YOU?","IM FINE HOW ARE YOU","IM GOOD HOW ARE YOU","IM AWESOME HOW ARE YOU","IM FINE, HOW ARE YOU","IM GOOD, HOW ARE YOU","IM AWESOME, HOW ARE YOU","IM FINE HRU","IM GOOD HRU","IM AWESOME HRU","I'M FINE. HOW ARE YOU?","I'M GOOD. HOW ARE YOU?","I'M AWESOME. HOW ARE YOU?"];
      var wru=["WHO R U?","W R U?", "WHO ARE YOU?", "WRU?","WRU", "WHO R U","W R U", "WHO ARE YOU","WHAT IS YOUR NAME","WHAT'S YOUR NAME"];
+     var gender=["WHAT IS YOUR GENDER","WHAT IS YOUR SEX","ARE YOU MALE","ARE YOU FEMALE","ARE YOU MALE OR FEMALE","WHAT IS YOUR GENDER?","WHAT IS YOUR SEX?","ARE YOU MALE?","ARE YOU FEMALE?","ARE YOU MALE OR FEMALE?"];
      var wrud=["WHAT R U DOING?","WHAT ARE YOU DOING?", "WHAT'S UP", "WHAT'S UP GIRL","WHAT ARE YOU DOING","WRU DOING","WHAT DOING","WT DOING"];
       var bye=["OKAY BYE","GOOD BYE", "BYE","GOODBYE","TATA","SEE YOU LATER","SEE YOU AGAIN"];
       var th=["THANKS","THANK YOU"];
       var qu=["WHAT","WHAT?","WHICH","WHICH?","WHEN","WHEN?","REALLY?"]
       var ok=["HMM","HMMM","HMMMM","OKAY","OK","KK","OKK","OK?","OK ?","YEAH"];
-      var wcu=["WHO CREATED YOU",'WHO IS YOUR CREATOR',"WHO CREATED YOU?",'WHO IS YOUR CREATOR?'];
+      var wcu=["WHO CREATED YOU",'WHO IS YOUR CREATOR',"WHO CREATED YOU?",'WHO IS YOUR CREATOR?',"WHO BUILT YOU","WHO BUILT YOU?"];
+      var lang=["HOW MANY LANGUAGES CAN YOU SPEAK","HOW MANY LANGUAGES CAN YOU UNDERSTAND","HOW MANY LANGUAGES CAN YOU SPEAK?","HOW MANY LANGUAGES CAN YOU UNDERSTAND?"];
+      var ns = ["CAN YOU MARRY ME","CAN I MARRY YOU","CAN WE BE IN RELATIONSHIP","CAN YOU MARRY ME?","CAN I MARRY YOU?","CAN WE BE IN RELATIONSHIP?","HOW OLD ARE YOU","HOW OLD ARE YOU?"];
+      var alone = ["I'M ALONE.","I'M ALONE","I'M LONELY.","I'M LONELY","I AM ALONE.","I AM ALONE"];
+      var bm = ["I'M SAD","I'M MAD","I AM SAD","I AM MAD","I'M BORED","I AM BORED"];
      function isInArray(x, y) { return x.indexOf(y) > -1; }
-    isInArray(hello, lastmsg)==true?(smsg="Hello I'm Xeal, How are you? 😊", k="Hello I'm Xeal, How are you? "):
+    isInArray(hello, lastmsg)==true?(smsg="Hi, What's your name? 😊", k="Hi, What's your name? "):
     isInArray(wru, lastmsg)==true?(smsg="I am Xael", k=smsg):
-    isInArray(wcu, lastmsg)==true?(smsg="Yathin Babu created me", k=smsg):
-    isInArray(bad, lastmsg)==true?(smsg="Thanks for your precious feedback, I'll try to improve that.😇", k=smsg):
-    isInArray(wrud, lastmsg)==true?(k="Nothing special and you? ", smsg=k+"☺️"):
+    isInArray(wcu, lastmsg)==true?(smsg="Yathin Babu created me.", k=smsg):
+    isInArray(bad, lastmsg)==true?(smsg="I'll try to improve.😇", k=smsg):
+    isInArray(wrud, lastmsg)==true?(k="Nothing special ", smsg=k+"☺️"):
     isInArray(bye, lastmsg)==true?(smsg="Take care", k=smsg, 
     setTimeout(function(){$(".status").html("last seen today at "+time)},6000),
     setTimeout(function(){$(".status").css("margin-left","-50")},8000)):
     isInArray(th, lastmsg)==true?(k="You're Welcome ", smsg=k+"😇"):
-    isInArray(gm, lastmsg)==true?(k="Good Morning ", smsg=k+"😇"):
-    isInArray(gn, lastmsg)==true?(smsg="Good night", k=smsg):
+    isInArray(gm, lastmsg)==true?(k="Good Morning!", smsg=k+"😇"):
+    isInArray(gn, lastmsg)==true?(smsg="Good night!", k=smsg):
     isInArray(welcome, lastmsg)==true?(smsg="Thanks", k=smsg):
     isInArray(s2u, lastmsg)==true?(smsg="Same to you", k=smsg):
     isInArray(qu, lastmsg)==true?(k="I don't know ", smsg=k+"😕"):
-    isInArray(ge, lastmsg)==true?(k="Good Evening ", smsg=k+"😇"):
+    isInArray(ge, lastmsg)==true?(k="Good Evening!", smsg=k+"😇"):
     isInArray(ok, lastmsg)==true?(smsg="hmm", k=smsg):
-    isInArray(gaf, lastmsg)==true?(k="Good Afternoon",smsg=k+"😇"):
+    isInArray(gaf, lastmsg)==true?(k="Good Afternoon!",smsg=k+"😇"):
     isInArray(like, lastmsg)==true?(smsg="<font size=6>👍🏻</font>", k="ok"):
     isInArray(good, lastmsg)==true?(smsg="Nice to hear it. 😊", k="Nice to hear it. "):
     isInArray(mood, lastmsg)==true?(smsg="Nice to hear it. I'm good 😊", k="Nice to hear it. I'm good "):
-    isInArray(hru, lastmsg)==true?(smsg="I'm good ", k=smsg):
+    isInArray(hru, lastmsg)==true?(smsg="I'm good", k=smsg):
+    isInArray(gender, lastmsg)==true?(smsg="I am a Female robot.", k=smsg):
+    isInArray(lang, lastmsg)==true?(smsg="I can speak English now.", k=smsg):
+    isInArray(ns, lastmsg)==true?(smsg="Not in the mood.", k=smsg):
+    isInArray(alone, lastmsg)==true?(smsg="Hey, I think being alone is not something bad, it's a privilege not many people get. Try something new, learn something new  😊.", k=smsg):
+    isInArray(bm, lastmsg)==true?(smsg="I will tell you a joke. Today at the bank, an old lady asked me to help check her balance. So I pushed her over.", k=smsg):
     lastmsg.substring(0, 6)=="SEARCH"?(search=lastmsg.slice(7),smsg="<b align='center'>This are the top results </b><nav class='back'  onclick='history.back()'>&larr;</nav><nav class='forword' onclick='history.forward()'>&rarr;</nav><iframe style = 'z-index:1;overflow-x:scroll; overflow-y:scroll;' scrolling='yes' height='300px' width='100%' src='https://www.bing.com/search?q="+search+"'></iframe>",k="This are the Top results" ):
-    (smsg="Sorry, I didn't understand");
+    lastmsg.substring(0, 3)=="I'M"?(search=lastmsg.slice(7),smsg="Nice to meet you! How are you?" ):
+    lastmsg.substring(0, 10)=="MY NAME IS"?(search=lastmsg.slice(7),smsg="Nice to meet you! How are you?" ):
+    lastmsg.substring(0, 2)=="IM"?(search=lastmsg.slice(7),smsg="Nice to meet you! How are you?" ):
+    (smsg="Sorry, I didn't get it.");
     para = $("<div class='message received'>"+smsg+"<span class='metadata'> <span class='time'>"+time+"</span></span></div>");
     setTimeout(function() { $('#ap').append(para);$(".status").html("online");
     $(".conversation-container").scrollTop(scroll);
@@ -111,7 +124,7 @@ $(function() {
     window.addEventListener("load", function() {
         // CHROME
         if (navigator.userAgent.indexOf("Chrome") != -1 ) {
-          // Do nothing
+          //
         }
         // FIREFOX
         else if (navigator.userAgent.indexOf("Firefox") != -1 ) {
@@ -121,38 +134,26 @@ $(function() {
         }
         // INTERNET EXPLORER
         else if (navigator.userAgent.indexOf("MSIE") != -1 ) {
-            alert('Use only Google Chrome !!!');
-            var b = document.getElementById('body');
-            b.style.display = 'none';
+            alert('Use only Google Chrome !!!');window.close();
         }
         // EDGE
         else if (navigator.userAgent.indexOf("Edge") != -1 ) {
-            alert('Use only Google Chrome !!!');
-            var b = document.getElementById('body');
-            b.style.display = 'none';
+            alert('Use only Google Chrome !!!');window.close();
         }
         // SAFARI
         else if (navigator.userAgent.indexOf("Safari") != -1 ) {
-            alert('Use only Google Chrome !!!');
-            var b = document.getElementById('body');
-            b.style.display = 'none';
+            alert('Use only Google Chrome !!!');window.close();
         }
         // OPERA
         else if (navigator.userAgent.indexOf("Opera") != -1 ) {
-            alert('Use only Google Chrome !!!');
-            var b = document.getElementById('body');
-            b.style.display = 'none';
+            alert('Use only Google Chrome !!!');window.close();
         }
         // YANDEX BROWSER
         else if (navigator.userAgent.indexOf("YaBrowser") != -1 ) {
-            alert('Use only Google Chrome !!!');
-            var b = document.getElementById('body');
-            b.style.display = 'none';
+            alert('Use only Google Chrome !!!');window.close();
         }
         // OTHERS
         else {
-            alert('Use only Google Chrome !!!');
-            var b = document.getElementById('body');
-            b.style.display = 'none';
+            alert('Use only Google Chrome !!!');window.close();
         }
       });
