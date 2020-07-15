29d0c65b010053bc1f728fe113ab34fb3da43737
Yathin Babu N <anythingicode@gmail.com>
2020-07-15 13:23:56 +0530

    index header change

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ function myFunction() {
   var x = document.getElementById("nav-bar");
   if (x.className === "nav-bar") {
     x.className += " responsive";
-    x.style.backgroundColor = '#000';
   } else {
     x.className = "nav-bar";
   }
